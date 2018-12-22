818f20f2b7f355c9511989eb5cb69938fc25ee75
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2018-12-22 14:38:03 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pxt-practice
+﻿# practice
+
+Practice extension blocks for makecode.
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+practice=github:CXPhoenix/pxt-practice
+```
---
+++ main.ts
@@ -0,0 +1,32 @@
+﻿//% weight=0 color=#00e3e3 icon="\uf40d" block="Practice"
+namespace practice {
+    /**
+    * 指定伺服馬達慢慢轉動一定角度
+    */
+    
+    //% blockId="servoMovement" block="servo pin %pin|angle from %angle1|to %angle2|%times secs"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function servoMovement(pin: AnalogPin, angle1: number = 0, angle2: number = 180, times: number = 1): void {
+        let turn = 0
+        let pauseTime = 0
+
+        if (angle1 < angle2) {
+            turn = 1
+        } else if (angle1 == angle2) {
+            turn = 0
+        } else {
+            turn = -1
+        }
+
+        pauseTime = (((angle1-angle2)*turn*-1)/times)*1000
+
+        
+        if (angle1 != angle2) {
+            for (let i = angle1; i <= angle2; i += i*turn) {
+                pins.servoWritePin(pin, i)
+                basic.pause(pauseTime)
+
+            }
+        }
+    }
+}
