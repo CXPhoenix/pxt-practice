d0bb3a61f8b3ce6a870393256360e25367eddc95
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2018-12-22 14:39:59 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace practice {
     * 指定伺服馬達慢慢轉動一定角度
     */
     
-    //% blockId="servoMovement" block="servo pin %pin|angle from %angle1|to %angle2|%times secs"
+    //% blockId="servoMovement" block="servo pin %pin|angle from %angle1|to %angle2|in %times secs"
     //% blockGap=2 weight=0 blockExternalInputs=true
     export function servoMovement(pin: AnalogPin, angle1: number = 0, angle2: number = 180, times: number = 1): void {
         let turn = 0
